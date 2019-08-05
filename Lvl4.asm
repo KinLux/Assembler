87c37259403c0a8607f917bc0f5a62604e7aeb90
kevin <kevin.k.hessen@gmail.com>
2019-08-05 14:40:32 +0200

    Create Lvl4.asm

---
+++ Lvl4.asm
@@ -0,0 +1,16 @@
+section .text
+
+global _start
+
+_start:
+    mov ebx, 1
+    mov ecx, 4
+
+label:
+    add ebx, ebx; 1+=1, 2+=2,  4+=4, 8+=8
+    dec ecx
+    cmp ecx, 0
+    jg label
+
+    mov eax, 1
+    int  0x80
