1c748866b1d77b20a529b6805c2bc3b2d6bb655b
kevin <kevin.k.hessen@gmail.com>
2019-08-05 14:06:41 +0200

    Update Lvl1.asm

---
+++ Lvl1.asm
@@ -0,0 +1,14 @@
+section .data
+  msg db "Hello, world!", 0x0a
+  len equ $ - msg
+
+section .text
+_start:
+  mov eax, 4; sys_write
+  mov ebx, 1; file descriptor
+  mov ecx, msg; bytes to sys_write
+  mov edx, len; number of bytes to sys_write
+  int 0x80
+  mov eax, 1; sys_exit
+  mov ebx, 0; exit status 0
+  int  0x80
